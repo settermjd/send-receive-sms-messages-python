31c292045a463e373fa66b2eb93cc2a3514bdcc8
Matthew Setter <matthew@matthewsetter.com>
2026-04-29 10:09:30 +1000

    Improve the documentation in send_sms.py
    This change improves the documentation so that the intent of the code is
    clearer. It also renames the file to follow Python naming standards.

--- send_sms.py
+++ send_sms.py
@@ -1,19 +1,22 @@
 # /usr/bin/env python
+# -*- coding: utf-8 -*-
+"""Show how to send an SMS with Python and Twilio"""
+
 import os
+
 from dotenv import load_dotenv
 from twilio.rest import Client
 
 load_dotenv()
 
-# Find these values at https://twilio.com/user/account
-# To set up environmental variables, see http://twil.io/secure
+# You can find the first three of these values at
+# https://twilio.com/user/account
 account_sid = os.environ['TWILIO_ACCOUNT_SID']
 auth_token = os.environ['TWILIO_AUTH_TOKEN']
-recipient = os.environ['RECIPIENT']
 sender = os.environ['TWILIO_PHONE_NUMBER']
+recipient = os.environ['RECIPIENT']
 
 client = Client(account_sid, auth_token)
-
 client.api.account.messages.create(
     to=recipient,
     from_=sender,
